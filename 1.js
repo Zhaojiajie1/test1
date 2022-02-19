45f63fa5b4c48de7942099f5daaa595b48f40f5b
Zhaojiajie <2875854612@qq.com>
2022-02-19 18:19:52 +0800

    orz

---
+++ 1.js
@@ -0,0 +1,286 @@
+//轮播图的js
+let items = document.getElementsByClassName("item")
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let points = document.getElementsByClassName("point")
+let time = 0;
+let index = 0;
+
+var Clear = function () {
+    for (i = 0; i < items.length; i++) {
+        items[i].className = "item";
+        points[i].className = "point";
+    }
+}
+var goIndex = function () {
+    Clear();
+    items[index].className = "item active";
+}
+btn1.onclick = function goPre() {
+    if (index == 0) index = items.length - 1;
+    else index--;
+    goIndex();
+    points[index].className = "point active";
+    time = 0;
+}
+var goNext = function () {
+    if (index == items.length - 1) index = 0;
+    else index++;
+    goIndex();
+    points[index].className = "point active";
+    time = 0;
+}
+btn2.onclick = function goNext() {
+    if (index == items.length - 1) index = 0;
+    else index++;
+    goIndex();
+    points[index].className = "point active";
+    time = 0;
+};
+setInterval(function () {
+    time++;
+    if (time == 10) {
+        goNext();
+        time = 0;
+    }
+}, 200)
+
+//商品的js
+let assessments = document.getElementsByClassName("assessment");
+let goods = document.getElementsByClassName("div19");
+
+/* assessments[0].style.display = "block";
+setTimeout(function () {
+    assessments[0].style.transform = "translateY(-90px)";
+}, 300) */
+
+let i = 0;
+function show(dom) {
+    setTimeout(function () {
+        dom.style.transform = "translateY(-90px)";
+    }, 100)
+}
+
+for (i = 0; i < goods.length; i++) {
+    goods[i].index = i;
+    goods[i].addEventListener("mouseenter", function () {
+        let j = this.index;
+        show(assessments[j]);
+
+        for (let k = 0; k < goods.length; k++) {
+            goods[k].index = k;
+            goods[k].addEventListener("mouseleave", function () {
+                j = this.index;
+                setTimeout(function () {
+                    assessments[j].style.transform = "translateY(90px)";
+                }, 100)
+            })
+        }
+
+    })
+}
+//主体部分高度设置
+let box = document.getElementsByClassName('box')[0];
+box.style.height = document.getElementsByClassName('show')[0].offsetHeight + 'px';
+
+//页面跳转
+let childBox = document.getElementsByClassName('box')[0].children;
+let toRegister = document.getElementsByClassName('toRegister')[0];
+let toLogin = document.getElementsByClassName('toLogin')[0];
+let Register = document.getElementsByClassName("register")[0];
+let login = document.getElementsByClassName('login')[0];
+let body = document.getElementsByClassName('body')[0];
+let toBody = document.getElementsByClassName('toBody')[0];
+let toCart = document.getElementsByClassName('toCart')[0];
+let cart = document.getElementsByClassName('cart')[0];
+//注册
+toRegister.addEventListener('click', function () {
+    for (let i = 0; i < childBox.length; i++) {
+        childBox[i].classList.remove('show');
+    }
+    Register.className = Register.className + ' ' + 'show';
+
+    box.style.height = document.getElementsByClassName('show')[0].offsetHeight + 'px';
+})
+//登录
+toLogin.addEventListener('click', function () {
+    for (let i = 0; i < childBox.length; i++) {
+        childBox[i].classList.remove('show');
+    }
+    login.className = login.className + ' ' + 'show';
+
+    box.style.height = document.getElementsByClassName('show')[0].offsetHeight + 'px';
+})
+//小米商城
+toBody.addEventListener('click', function () {
+    for (let i = 0; i < childBox.length; i++) {
+        childBox[i].classList.remove('show');
+    }
+    body.className = body.className + ' ' + 'show';
+
+    box.style.height = document.getElementsByClassName('show')[0].offsetHeight + 'px';
+})
+
+//注册部分JS
+let creatUsername = document.getElementById('creatUsername');
+let creatPassword = document.getElementById('creatPassword');
+let creatName = document.getElementById('creatName');
+let creat = document.getElementById('creat');
+
+creat.onclick = function () {
+    ajax({
+        type: "POST",
+        url: "http://180.76.185.37:3000/register",
+        data: {
+            username: creatUsername.value,
+            password: creatPassword.value,
+            name: creatName.value,
+        },
+        async: true,
+        success: function (res) {
+            res = JSON.parse(res);
+            console.log(res);
+            if (res.state == "0") {
+                alert(res.message);
+            }
+            else {
+                alert(res.message);
+                for (let i = 0; i < childBox.length; i++) {
+                    childBox[i].classList.remove('show');
+                }
+                login.className = login.className + ' ' + 'show';
+
+                box.style.height = document.getElementsByClassName('show')[0].offsetHeight + 'px';
+            }
+        }
+    })
+}
+//登录部分JS
+let loginUsername = document.getElementById('loginUsername');
+let loginPassword = document.getElementById('loginPassword');
+let loginBtn = document.getElementById('login');
+let admin = document.getElementById('admin');
+let auth = 'user';
+//用户token
+let token = '';
+//用户name
+let name = '';
+
+loginBtn.onclick = function () {
+    if (admin.checked) {
+        auth = 'admin';
+    }
+    ajax({
+        type: 'post',
+        url: "http://180.76.185.37:3000/login",
+        data: {
+            username: loginUsername.value,
+            password: loginPassword.value,
+            auth: auth,
+        },
+        success: function (res) {
+            res = JSON.parse(res);
+            console.log(res);
+            if (res.state === 1) {
+                alert(res.message);
+                token = res.token;
+                console.log(token);
+                name = res.name;
+                for (let i = 0; i < childBox.length; i++) {
+                    childBox[i].classList.remove('show');
+                }
+                body.className = body.className + ' ' + 'show';
+
+                box.style.height = document.getElementsByClassName('show')[0].offsetHeight + 'px';
+                toLogin.innerText = "用户名:" + name;
+            }
+        }
+    })
+}
+//插入购物车
+let buyBtn = document.getElementsByClassName('buy');
+let gid = '';
+let cartnumber = 0;
+
+//添加购物车（购买操作）
+for (let i = 0; i < buyBtn.length; i++) {
+    buyBtn[i].addEventListener('click', function () {
+        console.log(token);
+        gid = this.parentNode.getAttribute('data-gid');
+        //商品信息
+        let goodsname = document.getElementsByClassName('goodsname')[gid].innerText;
+        let goodsbrand = document.getElementsByClassName('goodsbrand')[gid].innerText;
+        let goodsprice = document.getElementsByClassName('goodsprice')[gid].innerText;
+        let goodsimg = document.getElementsByClassName('goodsimg')[gid];
+        goodsprice = goodsprice.substr(0, goodsprice.length - 1);
+        let cartnum = document.getElementsByClassName('cart-num')[0];
+
+
+        //向购物车中添加数据
+        ajax({
+            type: 'post',
+            url: 'http://180.76.185.37:3000/addCartData',
+            data: {
+                token: token,
+                data: {
+                    id: gid,
+                    name: goodsname,
+                    brand: goodsbrand,
+                    imageUrl: goodsimg.src,
+                    sales: '10',//暂时的
+                    cost: goodsprice,
+                    color: 'pink',//暂时暂时
+                },
+            },
+            async: true,
+            success: function (res) {
+                res = JSON.parse(res);
+                console.log(res);
+                alert(res.message);
+                if (res.state == 1) {
+                    cartnumber++;
+                    cartnum.innerText = cartnumber;
+                }
+            }
+        })
+    })
+}
+
+
+//封装一个ajax
+function ajax(opt) {
+    let defaultParam = {
+        type: 'get',
+        url: '#',
+        data: {},
+        async: true,
+        success: function () { },
+    }
+
+    for (key in opt) {
+        defaultParam[key] = opt[key];
+    }
+    let paramStr = "";
+    for (key in defaultParam.data) {
+        paramStr += key + '=' + defaultParam.data[key] + '&';
+    }
+    paramStr = paramStr.substr(0, paramStr.length - 1);
+
+    let xhr = new XMLHttpRequest();
+    if (defaultParam.type == 'get') {
+        xhr.open(defaultParam.type, defaultParam.url + '?' + paramStr, defaultParam.async);
+    } else {
+        xhr.open(defaultParam.type, defaultParam.url, defaultParam.async);
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.send(JSON.stringify(defaultParam.data));
+    }
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                defaultParam.success(xhr.responseText);
+            }
+        }
+    }
+
+}
---
+++ cart.js
@@ -0,0 +1,214 @@
+
+//查看购物车（跳转到购物车）
+toCart.addEventListener('click', function () {
+    ajax({
+        type: 'POST',
+        url: 'http://180.76.185.37:3000/getCartData',
+        data: {
+            token: token,
+        },
+        async: true,
+        success: function (res) {
+            res = JSON.parse(res);
+            console.log(res);
+            //用户验证成功
+            if (res.state == 1) {
+                for (let ii = 0; ii < childBox.length; ii++) {
+                    childBox[ii].classList.remove('show');
+                }
+                cart.className = cart.className + ' ' + 'show';
+                //购物车页面
+                let table = document.querySelector('table');
+                //获取商品
+                res.result.map(item => {
+
+                    //创建一行
+                    let tr = document.createElement('tr');
+                    //插入到table内
+                    table.appendChild(tr);
+                    tr.setAttribute('gata-gid', item.id);
+                    console.log(tr.getAttribute('gata-gid'));
+                    //创建选择列
+                    let tdOfCheck = document.createElement('td');
+                    let checkboxInput = document.createElement('input');
+                    checkboxInput.type = "checkbox";
+                    checkboxInput.className = "checkbox";
+                    tr.appendChild(tdOfCheck);
+                    tdOfCheck.appendChild(checkboxInput);
+
+                    //创建商品信息列（图片，名字，尺码）；
+                    let td2 = document.createElement("td");
+                    let img = document.createElement("img");
+                    let goodsName = document.createElement("p");
+                    goodsName.className = "goodsName";
+                    let spanOfChinese = document.createElement("span");
+                    let spanOfBrand = document.createElement("span");
+                    spanOfBrand.className = "size";
+                    tr.appendChild(td2);
+                    td2.appendChild(img);
+                    td2.appendChild(goodsName);
+                    td2.appendChild(spanOfChinese);
+                    spanOfChinese.innerText = "品牌:"
+                    td2.appendChild(spanOfBrand);
+                    img.className = "goodsimg";
+                    img.src = item.imageUrl;
+                    goodsName.innerText = item.name;
+                    spanOfBrand.innerText = item.brand;
+
+                    //创建单价列
+                    let tdOfPrice = document.createElement("td");
+                    let pOfPrice = document.createElement("p");
+                    pOfPrice.className = 'goodsPrice';
+                    tr.appendChild(tdOfPrice);
+                    tdOfPrice.appendChild(pOfPrice);
+                    pOfPrice.innerText = item.cost;
+
+                    //创建增减按钮列
+                    let tdOfBtn = document.createElement("td");
+                    let deletebtn = document.createElement("button");
+                    deletebtn.className = 'deletebtn';
+                    deletebtn.innerText = '-';
+                    let numberInput = document.createElement("input");
+                    numberInput.type = "text";
+                    numberInput.className = "number";
+                    numberInput.value = '1';
+                    let plus = document.createElement("button");
+                    plus.className = "plusbtn";
+                    plus.innerText = '+';
+
+                    tr.appendChild(tdOfBtn);
+                    tdOfBtn.appendChild(deletebtn);
+                    tdOfBtn.appendChild(numberInput);
+                    tdOfBtn.appendChild(plus);
+
+                    //创建小计列               
+                    let tdOfCount = document.createElement("td");
+                    let pOfCount = document.createElement("p");
+                    pOfCount.className = "count";
+                    tr.appendChild(tdOfCount);
+                    tdOfCount.appendChild(pOfCount);
+                    //计算小计
+                    pOfCount.innerText = numberInput.value * item.cost;
+
+                    //创建删除列
+                    let tdOfDelete = document.createElement("td");
+                    let aOfDelete = document.createElement("a");
+                    aOfDelete.href = 'javascript:void(0)';
+                    aOfDelete.className = "delete";
+                    tr.appendChild(tdOfDelete);
+                    tdOfDelete.appendChild(aOfDelete);
+                    aOfDelete.innerText = "删除";
+                })
+
+                //购物车操作
+                //删除操作
+                let remove = document.getElementsByClassName("delete");
+                for (let ii = 0; ii < remove.length; ii++) {
+                    remove[ii].addEventListener("click", function () {
+                        this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+                        let idx = this.parentNode.parentNode.getAttribute('gata-gid');
+                        ajax({
+                            type: "POST",
+                            url: 'http://180.76.185.37:3000/deleteCartData',
+                            data: {
+                                token: token,
+                                id: idx,
+                            },
+                            success: function (res) {
+                                res = JSON.parse(res);
+                                console.log(res);
+                                alert(res.message);
+                            }
+                        })
+                    })
+
+                }
+
+                //计算数量
+                let number = document.getElementsByClassName("number");
+                let plusbtn = document.getElementsByClassName("plusbtn");
+                let minusbtn = document.getElementsByClassName("deletebtn");
+                let count = document.getElementsByClassName("count");
+                let index1 = 0;
+                let ii = 0
+                let sum = 0;
+                let price = document.getElementsByClassName("goodsPrice");
+                // console.log(price[index1].innerText);
+                //console.log(number[index1].value);
+                count[index1].innerText = number[index1].value * (price[index1]).innerText;
+                // console.log(count[index1].innerText);
+
+
+                //计算总数，总价
+                let total = document.getElementsByClassName("total")[0];
+                total.innerText = number.length;
+                let totalMoney = document.getElementsByClassName('total-money')[0];
+                totalMoney.innerText = 0;
+
+                //选择操作
+                let checkbox = document.getElementsByClassName("checkbox");
+                for (ii = 0; ii < checkbox.length; ii++) {
+                    checkbox[ii].onclick = function () {
+                        let index1 = this.parentNode.parentNode.getAttribute("data-gid");
+                        console.log(index1);
+                        if (checkbox[index1].checked) {
+                            sum += parseInt(count[index1].innerText);
+                        }
+                        else {
+                            sum -= parseInt(count[index1].innerText);
+                        }
+                        totalMoney.innerText = sum;
+
+                    }
+                }
+                //增加操作
+                for (ii = 0; ii < plusbtn.length; ii++) {
+                    plusbtn[ii].addEventListener("click", function () {
+                        index1 = this.parentNode.parentNode.getAttribute("data-gid");
+                        console.log(index1);
+                        number[index1].value++;
+                        //计算小计
+                        //price = price[index1].innerText;
+                        count[index1].innerText = number[index1].value * price[index1].innerText;
+                        if (checkbox[index1].checked) {
+                            sum += parseInt(price[index1].innerText);
+                            totalMoney.innerText = sum;
+                        }
+
+                    })
+                }
+
+                //减少操作
+                for (ii = 0; ii < minusbtn.length; ii++) {
+                    minusbtn[ii].addEventListener("click", function () {
+                        index1 = this.parentNode.parentNode.getAttribute("data-gid");
+                        console.log(index1);
+                        if (number[index1].value == 1) {
+                            if (checkbox[index1].checked) {
+                                sum -= parseInt(price[index1].innerText);
+                                console.log(sum);
+                                totalMoney.innerText = sum;
+                            }
+                            this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+                            console.log(price[index1].innerText);
+                        }
+                        else {
+                            number[index1].value--;
+
+                            if (checkbox[index1].checked) {
+                                console.log(sum);
+                                sum -= parseInt(price[index1].innerText);
+                                console.log(sum);
+                                totalMoney.innerText = sum;
+                            }
+                        }
+                        //计算小计
+                        count[index1].innerText = number[index1].value * price[index1].innerText;
+                    })
+                }
+            }
+        }
+    })
+})
+
+
